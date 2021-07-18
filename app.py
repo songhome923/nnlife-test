d774f737450b453d15841677122de9a8143f000c
songhome923 <38522095+songhome923@users.noreply.github.com>
2021-07-18 14:47:06 +0900

    a

--- app.py
+++ app.py
@@ -21,17 +21,6 @@ tasks = [
 def home():
   return "Hello, World!"
 
-# #post /store data: {name :}
-# @app.route('/task' , methods=['POST'])
-# def create_store():
-#   request_data = request.get_json()
-#   new_store = {
-#     'name':request_data['name'],
-#     'items':[]
-#   }
-#   stores.append(new_store)
-#   return jsonify(new_store)
-  #pass
 
 #get /store
 @app.route('/task')
